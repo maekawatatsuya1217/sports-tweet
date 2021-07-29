7ca9fa51f46eccd53d53850ebdf3807dbfa5b5bf
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-29 13:05:58 +0900

    tweetモデルのバリデーションを編集

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,12 @@
 class Tweet < ApplicationRecord
+
+    has_one_attached :image
+
+    with_options presence: true do
+        validates :title
+        validates :catch_copy
+        validates :article
+        validates :image
+    end
+
 end
