a02c27830cd53447ba5a658a23c7a965f35856cc
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-07-29 12:52:29 +0900

    マイグレーションファイルの編集

--- db/migrate/20210729034429_create_tweets.rb
+++ db/migrate/20210729034429_create_tweets.rb
@@ -1,7 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-
+      t.string :title
+      t.string :catch_copy
+      t.text :article
       t.timestamps
     end
   end
